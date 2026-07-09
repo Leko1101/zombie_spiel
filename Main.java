5779f6a0494ccc49493225e5874df9991d9dbf8b
Leko1101 <heidenreich.leondd@gmail.com>
2026-07-09 12:42:40 +0200

    added player class

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player(920, 500, 20);
+    }
+        
+}
---
+++ Player.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import src.code.Zeichenfenster;
+
+public class Player {
+    int[] x;
+    int[] y;
+    Color color = Color.RED;
+    int size;
+
+    public Player(int x, int y, int size) {
+        this.x = new int[2];
+        this.y = new int[2];
+        this.x[0] = x;
+        this.x[1] = x + size;
+        this.y[0] = y;
+        this.y[1] = y + size;
+        this.size = size;
+    }
+
+    public void show(Zeichenfenster z) {
+        z.fillRectangle(x, y, color);
+    }
+
+}
